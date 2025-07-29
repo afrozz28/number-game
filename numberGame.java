4511a07d5169cf9658535f3749253aa44ccad17e
afrozz28 <afrozmujawar194@gmail.com>
2025-07-29 20:07:15 +0530

    Game is raedy

--- numberGame.java
+++ numberGame.java
@@ -7,48 +7,50 @@ public class numberGame {
         Random rand = new Random();
         int min = 1;
         int max = 100;
-        int maxAttempt = 6;
+        int maxAttempt = 8;
         int score = 0;
-        boolean playAgain = false, guessCorrectly = false;
+        boolean playAgain = false, guessCorrectly;
 
-        int attempts = 0;
+        int attempts;
 
-        int randomNum = rand.nextInt(max - min + 1) + min;
-        // int randomNum = rand.nextInt(101); also correct
-
-        System.out.println("🎮 Welcome to the Number Guessing Game!");
-
-        System.out.println();
+        System.out.println("🎮 Welcome to the Number Guessing Game!\n");
 
         System.out.println("Guess the number from " + min + " to " + max);
+        System.out.println("You have " + maxAttempt + " attempts.\n");
         do {
+            guessCorrectly = false;
+            attempts= 0;
+            int randomNum = rand.nextInt(max - min + 1) + min;
+
             while (attempts < maxAttempt) {
                 attempts++;
+                System.out.println();
                 System.out.print(attempts + ".Enter a number: ");
                 int num = sc.nextInt();
                 if (num > randomNum) {
-                    System.out.println("Number to high " + num);
+                    System.out.println("Number to high " + num + "\n");
                 } else if (num < randomNum) {
-                    System.out.println("Number is to low " + num);
+                    System.out.println("Number is to low " + num + "\n");
                 } else {
                     System.out.println("Congratulations! You guessed the number in " + attempts + " attempts.");
                     score++;
+                    attempts = 0;
                     guessCorrectly = true;
                     break;
                 }
 
             }
             if (!guessCorrectly) {
-                System.out.println("You've used all attempts you failled to guess the number. The number was: " + randomNum);
+                System.out.println( "You've used all attempts you failled to guess the number. The number was: " + randomNum);
             }
-            System.out.println("Do you want to play again? (yes/no): ");
+            System.out.print("Do you want to play again? (yes/no): ");
             String res = sc.next().toLowerCase();
             playAgain = res.equals("yes");
+
         } while (playAgain);
 
-        System.out.println();
         System.out.println("\nFinal Score: " + score);
-        System.out.println("Thanks for playing!");
+        System.out.println("Thanks for playing!\n");
         sc.close();
     }
 
